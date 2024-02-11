481b9f23e368f23c86f594204564a4056df84399
Flavio Macedo <fhenrique609@gmail.com>
2024-02-11 15:13:49 -0300

    Dashboard - Sidebar color e Card Status

--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from plataforma.models import RegistroFuncionarios, Status
 
 
 def base_admin(request):
-    return render(request, 'base_admin.html')
+    registros = RegistroFuncionarios.objects.all()
+    status_total = (
+        registros.filter(status=Status.CONCLUIDO).count() + 
+        registros.filter(status=Status.EM_ANALISE).count() +
+        registros.filter(status=Status.NAO_INICIADO).count() +
+        registros.filter(status=Status.PENDENTE).count() +
+        registros.filter(status=Status.SUSPENSO).count()
+    )
+
+    registros_concluidos = RegistroFuncionarios.objects.filter(status=Status.CONCLUIDO).count()
+
+    registros_pendentes = registros.filter(status=Status.PENDENTE).count() 
+
+    registros_em_analises = registros.filter(status=Status.EM_ANALISE).count()
+
+    registros_nao_iniciados = registros.filter(status=Status.NAO_INICIADO).count()
+
+    registros_suspensos = registros.filter(status=Status.SUSPENSO).count()
+
+    context = {
+        'registros': registros,
+        'status_total': status_total,
+        'registros_concluidos': registros_concluidos,
+        'registros_pendentes': registros_pendentes,
+        'registros_em_analises': registros_em_analises,
+        'registros_nao_iniciados': registros_nao_iniciados,
+        'registros_suspensos': registros_suspensos,
+    }
+
+    return render(request, 'tb_geral.html', context)

a8328acfe356a53c9a3b041b44e1faac660c4ea1
Flavio Macedo <fhenrique609@gmail.com>
2024-02-14 10:33:40 -0300

    Atualizar settings

--- cacex/settings.py
+++ cacex/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'cacex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('ENGINE'),
-        'NAME': config('NAME'),
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
         'USER': 'postgres',
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 

402ce88131de0c15852b6d7f569bc1dc102cb928
Flavio Macedo <fhenrique609@gmail.com>
2024-03-11 23:17:30 -0300

    Correção para colocar no deploy da fly.io

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/cacex/Documentos/Projetos da CACEX/CACEX v1 - Python Django/CacexV2/venv"
+set -gx VIRTUAL_ENV "/home/flavio/Documentos/CACEX v2 - Python Django/Projetos da CACEX/CACEX v1 - Python Django/CacexV2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

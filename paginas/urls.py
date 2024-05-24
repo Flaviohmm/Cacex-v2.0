ede4bacf3f2712aa5f4e50a4d7688f30551f9b52
Flavio Macedo <fhenrique609@gmail.com>
2024-05-24 09:16:51 -0300

    Gerar CSV/PDF - Tabela FNS/FNDE/SIMEC/ENTIDADE

--- paginas/urls.py
+++ paginas/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('gerar_pdf/', views.gerar_pdf, name='gerar_pdf'),
     path('gerar_pdf_caixa_municipios_selecionados/', views.gerar_pdf_caixa_municipios_selecionados, name='gerar_pdf_caixa_municipios_selecionados'),
     path('gerar_pdf_tabela_estado/', views.gerar_pdf_tabela_estado, name='gerar_pdf_tabela_estado'),
+    path('gerar_pdf_tabela_fnde/', views.gerar_pdf_tabela_fnde, name='gerar_pdf_tabela_fnde'),
+    path('gerar_pdf_tabela_simec/', views.gerar_pdf_tabela_simec, name='gerar_pdf_tabela_simec'),
+    path('gerar_pdf_tabela_fns/', views.gerar_pdf_tabela_fns, name='gerar_pdf_tabela_fns'),
+    path('gerar_pdf_tabela_entidade/', views.gerar_pdf_tabela_entidade, name='gerar_pdf_tabela_entidade'),
 ]
